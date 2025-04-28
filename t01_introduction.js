5ca88fa2aa2e3da201e97daafa55697227c474de
23129ps <23129ps@hvhs.school.nz>
2025-04-28 21:36:49 +0000

    Apr 28, 2025, 9:36 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,4 +1,19 @@
-
-
-
+/********************
+ Intro to JS
+ *******************/
 console.log("Running t01_introduction.js");
+
+//Variables
+
+
+/**********
+ Main Code
+ * **********/
+
+
+
+
+
+/**********
+ Main Code
+ * **********/
