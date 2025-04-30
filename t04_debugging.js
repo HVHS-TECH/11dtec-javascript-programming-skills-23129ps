bd0a12d507f26ae2371ec8dd32f128b980ac02db
23129ps <23129ps@hvhs.school.nz>
2025-04-30 00:21:07 +0000

    Apr 30, 2025, 12:21 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,3 +1,25 @@
+/********************
+ Intro to JS
+ Written by Priya Silwal
+ Written in 30/04/2025
+ Purpose: Using mathematical operations
+ *******************/
+
+ //Variables
+ let userName = "Riya";
+ let userAge = 10;
+ let pocketMoney = 40;
+ let currentYear = 2025;
+ /**********
+  Main Code
+  ***********/
+  console.log("Hi", userName);
+  console.log("As of", currentYear, "you are", userAge, "years old");
+  console.log("In 10 years you will be", userAge+10, "years old");
+  console.log("You have", pocketMoney, "dollars");
+  console.log("Half of your money is", pocketMoney/2);
+
+  
 konsole.log["Task 4 - Debugging")
 
 // calculate the area
@@ -20,3 +42,16 @@ if (perimeter > area){
 }else{
     console.log("Hmm, your perimeter is larger. It shouldn't be for this example)
 }
+
+--
+Welcome the user to the program.
+
+Ask the user their name.
+
+Ask the user their age.
+
+Ask the user how much pocket money they have.
+
+Then display the message from task 3 in an alert.
+
+
