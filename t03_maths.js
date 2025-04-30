7323af0b13b4a074e06fe57b13513240227f4422
23129ps <23129ps@hvhs.school.nz>
2025-04-30 00:05:52 +0000

    Apr 30, 2025, 12:05 AM

---
+++ t03_maths.js
@@ -0,0 +1,22 @@
+/********************
+ Intro to JS
+ Written by Priya Silwal
+ Written in 30/04/2025
+ Purpose: Using mathematical operations
+ *******************/
+
+ //Variables
+ let userName = "Riya";
+ let userAge = 10;
+ let pocketMoney = 40;
+ let currentYear = 2025;
+ /**********
+  Main Code
+  ***********/
+  console.log("Hi", userName);
+  console.log("As of", currentYear, "you are", userAge, "years old");
+  console.log("In 10 years you will be", userAge+10, "years old");
+  console.log("You have", pocketMoney, "dollars");
+  console.log("Half of your money is", pocketMoney/2);
+
+  
