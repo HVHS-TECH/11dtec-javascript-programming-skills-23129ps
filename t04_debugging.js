13a300060ed68ce4b2ae9e084f940cef20a38c1f
23129ps <23129ps@hvhs.school.nz>
2025-04-30 00:27:17 +0000

    Apr 30, 2025, 12:27 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -8,4 +8,24 @@
 console.log("Task 4 - Debugging");
 
 
+// Variables//
+var length = 8;
+var width = 10;
+
+// calculate the area
+var area = length + width;
+
+// calculate the perimeter
+var perimeter = 2*(length+width); 
+
+// Display the results
+console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area);
+console.log("The same piece of paper has a perimeter of "+perimeter);
+
+// Check, does this result look sensible
+if (perimeter > area){
+    console.log("Yay, your area is larger, this is right for this example")
+}else{
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example)
+}
 
