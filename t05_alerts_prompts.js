c3b679043dbb884eed8936570a71ba2894f0d640
23129ps <23129ps@hvhs.school.nz>
2025-05-01 01:25:41 +0000

    May 1, 2025, 1:25 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -4,19 +4,16 @@
  Written in 01/05/2025
  Purpose: Getting familiar with Alerts & prompts
  *******************/
- alert("Welcome to the website!");
- prompt("What is your age?");
- prompt("How much pocket money they have?");
- alert("Welcome to the website!");
-
+ alert("Welcome to my website!");
 
  //Variables
  var userName = prompt("What is your name?");
- var userAge;
- var money;
+ var userAge = prompt("What is your age?");
+ var money = prompt("How much pocket money they have?");
+ var currentYear = 2025;
  
  
- /**********
+/**********
   Main Code
 ***********/
  alert("Hi", userName);
@@ -24,6 +21,8 @@
  console.log("In 10 years you will be", userAge+10, "years old");
  console.log("You have", pocketMoney, "dollars");
  console.log("Half of your money is", pocketMoney/2);
+
+ 
 
 
  
