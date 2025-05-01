86d2c322a5c00bd99ffe1b4fe1f2b6ef305a76ab
23129ps <23129ps@hvhs.school.nz>
2025-05-01 00:49:34 +0000

    May 1, 2025, 12:49 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,31 +1,29 @@
 /********************
  Debugging
  Written by Priya Silwal
- Written in 30/04/2025
+ Written on 30/04/2025
  Purpose: Learning how to debug
- *******************/
+*******************/
 
 console.log("Task 4 - Debugging");
 
+// Variables
+let length = 8;
+let width = 10;
 
-// Variables//
-var length = 8;
-var width = 10;
+// Calculate the area
+var area = length * width;
 
-// calculate the area//
-var area = length*width;
-
-// calculate the perimeter//
-var perimeter = 2*(length+width); 
+// Calculate the perimeter
+var perimeter = 2 * (length + width);
 
 // Display the results
-console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area);
-console.log("The same piece of paper has a perimeter of "+perimeter);
+console.log("An area of a piece of paper with a length of " + length + ", and a width of " + width + " is " + area);
+console.log("The same piece of paper has a perimeter of " + perimeter);
 
 // Check, does this result look sensible
-if (perimeter > area){
+if (perimeter > area) {
     console.log("Yay, your area is larger, this is right for this example");
-}else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example);
+} else {
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
 }
-
