68577f4c62e2b6ab8b741ffdfd6b6aa149267c6d
23129ps <23129ps@hvhs.school.nz>
2025-05-01 00:59:32 +0000

    May 1, 2025, 12:59 AM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,25 @@
+/********************
+ Alerts & prompts
+ Written by Priya Silwal
+ Written in 01/05/2025
+ Purpose: Getting familiar with Alerts & prompts
+ *******************/
+ 
+
+ //Variables
+ var userName;
+ var userAge;
+ var money;
+ 
+ 
+ /**********
+  Main Code
+***********/
+ alert("What is your name?");
+ alert("What is your name?");
+ 
+ 
+ 
+ 
+ 
+ 
