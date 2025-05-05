e4e50f75e67e26612250c3568d7a4773a09043ea
23129ps <23129ps@hvhs.school.nz>
2025-05-05 21:09:11 +0000

    May 5, 2025, 9:09 PM

---
+++ t06_arrays.js
@@ -0,0 +1,32 @@
+/********************
+ Arrays part 1
+ Written by Priya Silwal
+ Written in 06/05/2025
+ Purpose: Getting familiar with Arrays
+ *******************/
+ alert("Welcome to my website!");
+
+ //Variables
+ var userName = prompt("What is your name?");
+ var userAge = Number(prompt("What is your age?"));
+ var money = Number(prompt("How much pocket money they have?"));
+ var currentYear = 2025;
+ 
+ 
+/**********
+  Main Code
+***********/
+  alert("Hi " + userName+".");
+  alert("As of "+ currentYear+ " you are "+ userAge+ " years old.");
+  alert("In 10 years you will be "+ (userAge+10) + " years old.");
+  alert("You have "+ money+ " dollars."); 
+  alert("Half of your money is "+ money/2+ " dollars.");
+
+ 
+
+
+ 
+ 
+ 
+ 
+ 
