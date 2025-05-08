7efc99fd31a53ca8c92cd4340f22213c217940c8
23129ps <23129ps@hvhs.school.nz>
2025-05-08 01:06:52 +0000

    May 8, 2025, 1:06 AM

---
+++ t10_activate_via_button.js
@@ -0,0 +1,39 @@
+/********************
+ Functions
+ Written by Priya Silwal
+ Written in 08/05/2025
+ Purpose: Getting familiar with Functions
+ *******************/
+ alert("Welcome to my website!");
+
+ /**********
+  Functions
+  ***********/
+ function askUserName ()  {
+   userName = prompt("What is your name?");
+   alert("Hi " + userName+".");
+ }
+
+ function askUserAge () {
+    userAge = Number(prompt("What is your age?"));
+ }
+
+ function askUserMoney  ()  {
+    money = Number(prompt("How much pocket money they have?"));
+    alert("You have "+ money+ " dollars."); 
+  if (money >= 20){
+    alert("You can afford a chocolate bar");
+  }
+ else {
+    alert("You can't afford a chocolate bar");
+ }
+ }
+
+/**********
+  Main Code
+***********/
+ askUserName();
+ askUserAge();
+ askUserMoney();
+ 
+ 
