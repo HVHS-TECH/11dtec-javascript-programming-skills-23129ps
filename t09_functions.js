695172506e86865fdb93e1d6100e34648091fc91
23129ps <23129ps@hvhs.school.nz>
2025-05-08 01:01:31 +0000

    May 8, 2025, 1:01 AM

--- t09_functions.js
+++ t09_functions.js
@@ -6,13 +6,6 @@
  *******************/
  alert("Welcome to my website!");
 
- //Variables
- var currentYear = 2025;
- let array =["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];
- let choice= prompt("On a scale of 0-3 how much do you like chocolate");
- let userTotal = 0;
- let userNumberChoice = 0;
- 
  /**********
   Functions
   ***********/
@@ -35,28 +28,10 @@
  askUserAge();
   money();
   alert("Hi " + userName+".");
-  alert("As of "+ currentYear+ " you are "+ userAge+ " years old.");
-  alert("In 10 years you will be "+ (userAge+10) + " years old.");
   alert("You have "+ money+ " dollars."); 
-  alert("Half of your money is "+ money/2+ " dollars.");
-  alert(array[choice]);
-  alert("I predict you were born in " + (currentYear-userAge));
   if (money >= 20){
     alert("You can afford a chocolate bar");
   }
-  
-  for (let count = 0; count<5; count++) {
-  userNumberChoice = prompt('Enter a number between 0 to 5');
-  userNumberChoice = Number(userNumberChoice);
-  userTotal = userTotal + userNumberChoice; 
-  }
-  
-  console.log('The total of all numbers is: ' + userTotal); 
-
-  
-
- 
- 
  
  
  
