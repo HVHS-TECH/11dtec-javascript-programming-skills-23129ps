79dc16b6de5283dafdea45f6f3ea344c070e2b98
23129ps <23129ps@hvhs.school.nz>
2025-05-27 10:41:39 +0000

    May 27, 2025, 10:41 AM

--- t12_while_loop.js
+++ t12_while_loop.js
@@ -5,90 +5,75 @@
  Purpose: While loop
 *******************/
 
-/**********
- Variables 
-***********/
-let userName; 
-let userAge; 
-let userMoney;
+/********************  
+**********  
+ variables   
+***********/  
 
-/**********
- Functions
-***********/
+let userName;  
+let userAge;  
+let userMoney;  
 
-function start() {
-    userName = askUserName();
-    userAge = askUserAge();
-    userMoney = askUserMoney();
-    restart();
-}
+/**********  
+ Functions  
+***********/  
+function start() {  
+    userName = askUserName();  
+    userAge = askUserAge();  
+    userMoney = askUserMoney();  
+}  
 
-function askUserName() {
-    alert("Welcome to my website!");
-    let name = prompt("What is your name?");
-    alert("Hi " + name + "!");
-    return name;
-}
+function askUserName() {  
+    alert("Welcome to my website!");  
+    userName = prompt("What is your name?");  
+    alert("Hi " + userName + " =!");  
+}  
 
-function askUserAge() {
-    let currentYear = 2025;
-    let age = Number(prompt("What is your age?"));
+function askUserAge() {  
+    currentYear = 2025;  
+    userAge = Number(prompt("What is your age?"));  
+    alert("I predict you were born in " + (currentYear - userAge) + ".");  
+}  
 
-    while (age < 0 || age > 130) {
-        age = Number(prompt("Invalid! You must enter a valid age between 0-130"));
-    }
-    
-    alert("I predict you were born in " + (currentYear - age) + ".");
-    return age;
-}
+function askUserMoney() {  
+    let array = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];  
+    let choice = prompt("On a scale of 0-3 how much do you like chocolate?");  
+    alert(array[choice]);  
+    userMoney = Number(prompt("How much pocket money do you have?"));  
+    alert("You have " + userMoney + " dollars.");  
 
-function askUserMoney() {
-    let array = ["You loathe chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];
-    let choice = Number(prompt("On a scale of 0-3 how much do you like chocolate?"));
+    if (userMoney >= 20) {  
+        alert("You can afford a chocolate bar");  
+    } else {  
+        alert("You can't afford a chocolate bar");  
+    }  
+}  
 
-    if (choice >= 0 && choice <= 3) {
-        alert(array[choice]);
-    } else {
-        alert("Invalid choice, please enter a number between 0 and 3.");
-    }
+/**********  
+ Learning Goal  
+***********/  
+// We are learning how to:  
+// Use a while loop to repeat a section of code while a condition is met.  
 
-    let money = Number(prompt("How much pocket money do you have?"));
-    alert("You have " + money + " dollars.");
-    
-    if (money >= 20) {
-        alert("You can afford a chocolate bar!");
-    } else {
-        alert("You can't afford a chocolate bar.");
-    }
-    
-    return money;
-}
+// By the end of this topic you will have:  
+// - Gone through the slides.  
+// - Copied your template from task 1.  
+// - Updated the header comments and console.log()  
+// - Created a program that includes while loops  
 
-// First while loop using user input
-function restart() {
-    let ask = prompt("Do you want to restart? (y/n)");
+/**********  
+ While Loops  
+***********/  
 
-    while (ask === "y") {
-        alert("Loop will repeat again");
-        start();
-    }
-    
-    alert("End of loop");
-}
+let userResponse = prompt("Would you like to continue with the loop? (y/n)");  
+while (userResponse === "y") {  
+    alert("Loop will repeat again");  
+    userResponse = prompt("Would you like to continue with the loop? (y/n)");  
+}  
+alert("End of loop");  
 
-// Second while loop using a boolean variable and confirm
-function restartWithConfirm() {
-    let continueLoop = confirm("Do you want to restart?");
-    
-    while (continueLoop) {
-        alert("Loop will repeat again");
-        continueLoop = confirm("Do you want to restart?");
-    }
-    
-    alert("End of loop");
-}
-
-// Call the functions
-start();
-restart();
-restartWithConfirm();
+// Second while loop using confirm statement  
+while (confirm("Would you like to continue with the loop?")) {  
+    alert("Loop will repeat again");  
+}  
+alert("End of loop");  
