b215e3a3adf5a05f3174fa8601b1b805c3df68a5
23129ps <23129ps@hvhs.school.nz>
2025-05-27 23:54:56 +0000

    May 27, 2025, 11:54 PM

--- t12_while_loop.js
+++ t12_while_loop.js
@@ -5,65 +5,71 @@
  Purpose: While loop
 *******************/
 
-/********************  
-**********  
- variables   
-***********/  
-
 let userName;  
 let userAge;  
 let userMoney;  
+let userResponse = "y";  // Initialize userResponse globally
 
 /**********  
  Functions  
-***********/  
+***********/
+
 function start() {  
     userName = askUserName();  
     userAge = askUserAge();  
     userMoney = askUserMoney();  
-}  
+}
 
 function askUserName() {  
     alert("Welcome to my website!");  
-    userName = prompt("What is your name?");  
-    alert("Hi " + userName + " !");  
-}  
+    let name = prompt("What is your name?");
+    alert("Hi " + name + " !");
+    return name;
+}
 
 function askUserAge() {  
-    currentYear = 2025;  
-    userAge = Number(prompt("What is your age?"));  
-    alert("I predict you were born in " + (currentYear - userAge) + ".");  
-}  
+    const currentYear = 2025;  
+    let age = Number(prompt("What is your age?"));
+    alert("I predict you were born in " + (currentYear - age) + ".");
+    return age;
+}
 
 function askUserMoney() {  
-    let array = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];  
-    let choice = prompt("On a scale of 0-3 how much do you like chocolate?");  
-    alert(array[choice]);  
-    userMoney = Number(prompt("How much pocket money do you have?"));  
-    alert("You have " + userMoney + " dollars.");  
+    let array = ["You loathe chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];
+    let choice = Number(prompt("On a scale of 0-3 how much do you like chocolate?"));
+   
+    // Validate choice to prevent errors
+    if (choice >= 0 && choice <= 3) {
+        alert(array[choice]);
+    } else {
+        alert("Invalid choice, please enter a number between 0-3.");
+    }
+   
+    let money = Number(prompt("How much pocket money do you have?"));
+    alert("You have " + money + " dollars.");
 
-    if (userMoney >= 20) {  
+    if (money >= 20) {  
         alert("You can afford a chocolate bar");  
     } else {  
         alert("You can't afford a chocolate bar");  
     }  
-}  
+   
+    return money;
+}
 
 /**********  
- While Loops  
-***********/  
+ While Loop  
+***********/
 
-let userResponse = "y";  
 while (userResponse === "y") {  
-    start(); 
-    alert("Loop will repeat again"); 
-    let userResponse = prompt("Would you like to continue with the loop? (y/n)");   
-}  
-if (userResponse === "n") {
-alert("End of loop"); 
-return;
+    start();  
+    alert("Loop will repeat again");  
+
+    // Remove `let` to update the global variable properly
+    userResponse = prompt("Would you like to continue with the loop? (y/n)");  
 }
- 
+
+alert("End of loop");  // Exit message when user enters 'n'
     
 
 
