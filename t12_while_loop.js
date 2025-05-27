451270448dc207760047b77f2bea170c5cc0a889
23129ps <23129ps@hvhs.school.nz>
2025-05-27 01:55:01 +0000

    May 27, 2025, 1:55 AM

--- t12_while_loop.js
+++ t12_while_loop.js
@@ -3,59 +3,77 @@
  Written by Priya Silwal
  Written in 08/05/2025
  Purpose: While loop
- *******************/
+*******************/
 
- 
- /**********
-  variables 
-  ***********/
+/**********
+ Variables 
+***********/
+let userName; 
+let userAge;
+let userMoney;
 
- let userName; 
- let userAge;
- let userMoney;
- let ask;
+/**********
+ Functions
+***********/
 
- /**********
-  Functions
-  ***********/
- function start () {
+function start() {
     userName = askUserName();
     userAge = askUserAge();
     userMoney = askUserMoney();
     restart();
- }
+}
 
- function askUserName ()  {
- alert("Welcome to my website!");
-  userName = prompt("What is your name?");
-  alert("Hi " + userName +" !");
- }
+function askUserName() {
+    alert("Welcome to my website!");
+    userName = prompt("What is your name?");
+    alert("Hi " + userName + "!");
+}
 
- function askUserAge () {
-  currentYear = 2025;
-  userAge =Number(prompt("What is your age?"));
-  alert("I predict you were born in " + (currentYear-userAge) + ".");
- }
+function askUserAge() {
+    let currentYear = 2025;
+    userAge = Number(prompt("What is your age?"));
+    alert("I predict you were born in " + (currentYear - userAge) + ".");
+}
 
- function askUserMoney  ()  {
-  let array =["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];
-  let choice= prompt("On a scale of 0-3 how much do you like chocolate?");
-  alert(array[choice]);
-  userMoney= Number(prompt("How much pocket money they have?"));
-    alert("You have "+ userMoney+ " dollars."); 
-  if (userMoney >= 20){
-    alert("You can afford a chocolate bar");
-  }
- else {
-    alert("You can't afford a chocolate bar");
- }
- }
-
-//restart//
- function restart () {
-    ask = prompt("Do you want to restart?");
-    console.log(ask);
-    while (ask =yes) {
-        start();
+function askUserMoney() {
+    let array = ["You loathe chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];
+    let choice = prompt("On a scale of 0-3 how much do you like chocolate?");
+    alert(array[choice]);
+    userMoney = Number(prompt("How much pocket money do you have?"));
+    alert("You have " + userMoney + " dollars.");
+    
+    if (userMoney >= 20) {
+        alert("You can afford a chocolate bar!");
+    } else {
+        alert("You can't afford a chocolate bar.");
     }
- } 
+}
+
+// First while loop using user input
+function restart() {
+    let ask = prompt("Do you want to restart? (y/n)").toLowerCase();
+    
+    while (ask === "y") {
+        alert("Loop will repeat again");
+        ask = prompt("Do you want to restart? (y/n)").toLowerCase();
+    }
+    
+    alert("End of loop");
+}
+
+// Second while loop using a boolean variable and confirm
+function restartWithConfirm() {
+    let continueLoop = confirm("Do you want to restart?");
+    
+    while (continueLoop) {
+        alert("Loop will repeat again");
+        continueLoop = confirm("Do you want to restart?");
+    }
+    
+    alert("End of loop");
+}
+
+// Call the functions
+start();
+restart();
+restartWithConfirm();
