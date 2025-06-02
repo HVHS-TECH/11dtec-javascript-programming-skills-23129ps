97d74b8233da0bd044aba79e74d6e5a5b9d4cded
23129ps <23129ps@hvhs.school.nz>
2025-06-02 23:58:49 +0000

    Jun 2, 2025, 11:58 PM

---
+++ t16_cancel_and_exit.js
@@ -0,0 +1,12 @@
+/*********************************************/
+// Task 16 - Exit
+//  Term 2 2025
+/*********************************************/
+
+alert("Welcome to my website!");
+var userName = prompt("What is your name?");
+if (userName == null){
+    return;
+}
+
+c
